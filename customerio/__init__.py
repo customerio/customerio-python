16ba6df2dcebab4c64415e1862e9f1139511543b
Daniel Krol <orblivion@gmail.com>
2013-07-30 16:01:42 -0700

    Encodes entire JSON string into request
    Also fixes bug in error message

--- customerio/__init__.py
+++ customerio/__init__.py
@@ -4,7 +4,7 @@ import base64
 import urllib
 from httplib import HTTPSConnection
 
-VERSION = (0, 1, 2, 'final', 0)
+VERSION = (0, 1, 3, 'final', 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
@@ -21,13 +21,6 @@ def get_version():
 class CustomerIOException(Exception):
     pass
 
-class ObjectEncoder(json.JSONEncoder):
-    def default(self, obj):
-        try:
-            return JSONEncoder.default(obj, **kwargs)
-        except:
-            return str(obj)
-
 class CustomerIO(object):
     def __init__(self, site_id=None, api_key=None, host=None, port=None, url_prefix=None):
         self.site_id = site_id
@@ -43,7 +36,7 @@ class CustomerIO(object):
         return '%s/customers/%s/events' % (self.url_prefix, customer_id)
 
     def send_request(self, method, query_string, data):
-        data = json.dumps(data, cls=ObjectEncoder)
+        data = json.dumps(data)
         http = HTTPSConnection(self.host, self.port)
         basic_auth = base64.encodestring('%s:%s' % (self.site_id, self.api_key)).replace('\n', '')
         headers = {
@@ -54,7 +47,7 @@ class CustomerIO(object):
         http.request(method, query_string, data, headers)
         result_status = http.getresponse().status
         if result_status != 200:
-            raise CustomerIOException('%s: %s %s' % (result_status, query_string, data_string))
+            raise CustomerIOException('%s: %s %s' % (result_status, query_string, data))
 
     def identify(self, **kwargs):
         url = self.get_customer_query_string(kwargs['id'])
@@ -62,9 +55,8 @@ class CustomerIO(object):
 
     def track(self, customer_id, name, **data):
         url = self.get_event_query_string(customer_id)
-        encoded_data = {
+        post_data = {
             'name': name,
+            'data': data,
         }
-        for key, value in data.iteritems():
-            encoded_data['data[%s]' % key] = value
-        self.send_request('POST', url, encoded_data)
+        self.send_request('POST', url, post_data)
