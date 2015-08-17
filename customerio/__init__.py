c808688b6d6044bf3679afc9a23afd85ad26c66c
Kara Leary <learykara@gmail.com>
2015-08-17 11:33:30 -0400

    update customerio version

--- customerio/__init__.py
+++ customerio/__init__.py
@@ -1,9 +1,14 @@
+from __future__ import division
+import json
 import base64
-import urllib
-from httplib import HTTPSConnection
+try:
+    from httplib import HTTPSConnection
+except ImportError:
+    from http.client import HTTPSConnection
+from datetime import datetime
 
+VERSION = (0, 1, 8, 'final', 0)
 
-VERSION = (0, 1, 2, 'final', 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
@@ -22,50 +27,99 @@ class CustomerIOException(Exception):
 
 
 class CustomerIO(object):
-    def __init__(self, site_id=None, api_key=None, host=None, port=None, url_prefix=None):
+
+    def __init__(self, site_id=None, api_key=None, host=None, port=None, url_prefix=None, json_encoder=json.JSONEncoder):
         self.site_id = site_id
         self.api_key = api_key
         self.host = host or 'track.customer.io'
         self.port = port or 443
         self.url_prefix = url_prefix or '/api/v1'
+        self.json_encoder = json_encoder
 
     def get_customer_query_string(self, customer_id):
+        '''Generates a customer API path'''
         return '%s/customers/%s' % (self.url_prefix, customer_id)
 
     def get_event_query_string(self, customer_id):
+        '''Generates an event API path'''
         return '%s/customers/%s/events' % (self.url_prefix, customer_id)
 
     def send_request(self, method, query_string, data):
-        encoded_data = {}
-        for key, value in data.items():
-            if isinstance(value, unicode):
-                encoded_data[key] = value.encode('utf8')
-            else:
-                encoded_data[key] = value
-        data_string = urllib.urlencode(encoded_data)
+        '''Dispatches the request and returns a response'''
+
+        data = json.dumps(self._sanitize(data), cls=self.json_encoder)
         http = HTTPSConnection(self.host, self.port)
-        basic_auth = base64.encodestring('%s:%s' % (self.site_id, self.api_key)).replace('\n', '')
+        auth = "{site_id}:{api_key}".format(site_id=self.site_id, api_key=self.api_key).encode("utf-8")
+        basic_auth = base64.b64encode(auth)
+
         headers = {
-            'Authorization': 'Basic %s' % basic_auth,
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': len(data_string),
+            'Authorization': b" ".join([b"Basic", basic_auth]),
+            'Content-Type': 'application/json',
+            'Content-Length': len(data),
         }
-        http.request(method, query_string, data_string, headers)
+
+        http.request(method, query_string, data, headers)
         response = http.getresponse()
         result_status = response.status
         if result_status != 200:
-            raise CustomerIOException('%s: %s %s' % (result_status, query_string, data_string))
+            raise CustomerIOException('%s: %s %s' % (result_status, query_string, data))
         return response.read()
 
-    def identify(self, **kwargs):
-        url = self.get_customer_query_string(kwargs['id'])
+    def identify(self, id, **kwargs):
+        '''Identify a single customer by their unique id, and optionally add attributes'''
+        url = self.get_customer_query_string(id)
         self.send_request('PUT', url, kwargs)
 
     def track(self, customer_id, name, **data):
+        '''Track an event for a given customer_id'''
         url = self.get_event_query_string(customer_id)
-        encoded_data = {
+        post_data = {
             'name': name,
+            'data': data,
         }
-        for key, value in data.iteritems():
-            encoded_data['data[%s]' % key] = value
-        self.send_request('POST', url, encoded_data)
+        self.send_request('POST', url, post_data)
+
+    def pageview(self, customer_id, page, **data):
+        '''Track a pageview for a given customer_id'''
+        url = self.get_event_query_string(customer_id)
+        post_data = {
+            'type': "page",
+            'name': page,
+            'data': data,
+        }
+        self.send_request('POST', url, post_data)
+
+    def backfill(self, customer_id, name, timestamp, **data):
+        '''Backfill an event (track with timestamp) for a given customer_id'''
+        url = self.get_event_query_string(customer_id)
+
+        if isinstance(timestamp, datetime):
+            timestamp = self._timedelta_to_timestamp(timestamp - datetime(1970, 1, 1))
+        elif not isinstance(timestamp, int):
+            try:
+                timestamp = int(timestamp)
+            except Exception as e:
+                raise CustomerIOException("{t} is not a valid timestamp ({err})".format(t=timestamp, err=e))
+
+        post_data = {
+            'name': name,
+            'data': data,
+            'timestamp': timestamp
+        }
+
+        self.send_request('POST', url, post_data)
+
+    def delete(self, customer_id):
+        '''Delete a customer profile'''
+
+        url = self.get_customer_query_string(customer_id)
+        self.send_request('DELETE', url, {})
+
+    def _sanitize(self, data):
+        for k, v in data.items():
+            if isinstance(v, datetime):
+                data[k] = self._timedelta_to_timestamp(v - datetime(1970, 1, 1))
+        return data
+
+    def _timedelta_to_timestamp(self, td):
+        return int((td.microseconds + (td.seconds + td.days * 24 * 3600) * 10**6) / 10**6)
