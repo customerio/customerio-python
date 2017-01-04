2ba3c6a04ba686b1aea1923bc7882937737290cf
Stephen Young <me@hownowstephen.com>
2017-01-04 12:31:02 -0500

    Better install (#30)
    move versioning code to setup.py and remove install requirement

--- customerio/__init__.py
+++ customerio/__init__.py
@@ -13,19 +13,6 @@ try:
 except ImportError:
     USE_PY3_TIMESTAMPS = False
 
-VERSION = (0, 2, 0, 'final', 0)
-
-def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    if VERSION[3:] == ('alpha', 0):
-        version = '%s pre-alpha' % version
-    else:
-        if VERSION[3] != 'final':
-            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
-    return version
-
 warnings.simplefilter("default")
 
 class CustomerIOException(Exception):
