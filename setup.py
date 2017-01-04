b516e8479b34632df38f91dec02aef7cf38c2965
Stephen Young <me@hownowstephen.com>
2017-01-04 12:28:17 -0500

    version bump

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 
-VERSION = (0, 2, 0, 'final', 0)
+VERSION = (0, 2, 1, 'final', 0)
 
 def get_version():
     version = '%s.%s' % (VERSION[0], VERSION[1])
