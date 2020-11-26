72c4bd6958741c387ed336592e241086d4d62e87
Swastik Agarwal <swastik@customer.io>
2020-11-26 17:48:11 +0530

    Remove some unnecessary imports from init

--- customerio/__init__.py
+++ customerio/__init__.py
@@ -1,19 +1,4 @@
-from __future__ import division
-from datetime import datetime
-import math
-import time
 import warnings
-
-from requests import Session
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-
-try:
-    from datetime import timezone
-    USE_PY3_TIMESTAMPS = True
-except ImportError:
-    USE_PY3_TIMESTAMPS = False
-
 
 from .client_base import CustomerIOException
 from .track import CustomerIO
