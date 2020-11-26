bf9723102d8566ed43ffcfb92301f73a2b99c6c3
Swastik Agarwal <swastik@customer.io>
2020-11-26 17:37:27 +0530

    Remove unused API key

--- customerio/api.py
+++ customerio/api.py
@@ -3,7 +3,7 @@ from .client_base import ClientBase
 
 class APIClient(ClientBase):
 
-    def __init__(self, key, api_key=None, url=None, retries=3, timeout=10, backoff_factor=0.02):
+    def __init__(self, key, url=None, retries=3, timeout=10, backoff_factor=0.02):
         self.url = url or 'https://api.customer.io'
         ClientBase.__init__(self, retries=retries,
                             timeout=timeout, backoff_factor=backoff_factor)
