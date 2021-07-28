b4113b926e117e2e04dddf4a5bdb2126a832047c
Lefteris Zafiris <zaf@fastmail.com>
2021-07-28 13:20:30 +0300

    Set the TCP keepalive socket options
    Configure the TCP keepalive timeout and interval in a way that matches
    our server side behaviour. opsbugs #2100

--- customerio/client_base.py
+++ customerio/client_base.py
@@ -4,10 +4,12 @@ Implements the base client that is used by other classes to make requests
 from __future__ import division
 from datetime import datetime, timezone
 import math
+import socket
 
 from requests import Session
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
+from urllib3.connection import HTTPConnection
 
 
 class CustomerIOException(Exception):
@@ -17,6 +19,14 @@ class ClientBase(object):
     def __init__(self, retries=3, timeout=10, backoff_factor=0.02):
         self.timeout = timeout
         self.retries = retries
+
+        # Set the TCP keepalive settings to the values dicated by our server-side configuration.
+        HTTPConnection.default_socket_options = ( HTTPConnection.default_socket_options + [
+            (socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1),
+            (socket.SOL_TCP, socket.TCP_KEEPIDLE, 550),
+            (socket.SOL_TCP, socket.TCP_KEEPINTVL, 60)
+            ]
+        )
 
         self.http = Session()
         # Retry request a number of times before raising an exception
