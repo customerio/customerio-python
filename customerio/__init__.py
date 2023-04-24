482324bed80bfa4b4dce64b01a842148a76f7c62
Colby Ausen <colby@customer.io>
2023-04-24 16:58:29 -0600

    Support sending transactional push messages

--- customerio/__init__.py
+++ customerio/__init__.py
@@ -2,5 +2,5 @@ import warnings
 
 from customerio.client_base import CustomerIOException
 from customerio.track import CustomerIO
-from customerio.api import APIClient, SendEmailRequest
+from customerio.api import APIClient, SendEmailRequest, SendPushRequest
 from customerio.regions import Regions
