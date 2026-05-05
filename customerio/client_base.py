c77bd64bd2ee4659c43cdb88883e9c706e194e38
Stephen Young <stephen@customer.io>
2026-05-05 13:56:34 -0400

    Bump Ruff and fix merge markers

--- customerio/client_base.py
+++ customerio/client_base.py
@@ -108,11 +108,6 @@ Last caught exception -- {type(e)}: {e}
         return session
 
     def _close(self):
-<<<<<<< ci-fixes
         if not self.use_connection_pooling and self._current_session is not None:
-=======
-        # if we're not using pooling; clean up the resources.
-        if (not self.use_connection_pooling and self._current_session is not None):
->>>>>>> main
             self._current_session.close()
             self._current_session = None
